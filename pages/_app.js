8a426fedf5bb7fa2e08639d93d41c1cb33f40598
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-10-10 11:36:24 +0200

    cambio de la estructura. components added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+// Components
+import Layout from '../components/Layout'
+
+// Styles
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
 
 export default MyApp
