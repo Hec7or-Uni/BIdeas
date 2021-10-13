8215ac009ca6e8876f7e6714547041ebec02e869
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-10-13 20:18:58 +0200

    banners del home

--- src/pages/home.js
+++ src/pages/home.js
@@ -60,10 +60,37 @@ export default function Home() {
                 >
                     {/* Tarjeta izquierda */}
                     <div
-                        className="h-full rounded-tl-xl rounded-bl-xl p-4 bg-gray-200"
+                        className="h-full rounded-tl-xl rounded-bl-xl p-4 bg-gray-200 tracking-wide relative"
                         style={{ width: '55%' }}
                     >
-                        .
+                        {/* Parte superior */}
+                        <div className="flex items-center">
+                            <div className="h-28 w-28 bg-red-500">
+                                img
+                            </div>
+                            <div className="flex flex-col ml-5">
+                                <p className="text-base font-bold capitalize">
+                                    entrepreneur
+                                </p>
+                                <hr className="h-1 w-3/4 my-1.5 bg-gray-700 rounded-full" />
+                                <p className="text-sm font-semibold">
+                                    0% towards your own boss
+                                </p>
+                            </div>
+                        </div>
+                        {/* Histoial */}
+                        {/* Rank */}
+                        <div className="flex items-center absolute bottom-0 mb-10">
+                            <p className="text-lg font-medium capitalize">
+                                rank up - 0
+                            </p>
+                            <div className="flex  items-center h-7 w-7 ml-3 rounded-full relative">
+                                <div className="w-full h-full rounded-full bg-green-200 absolute filter blur-sm" />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mx-auto rotate-180 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
                     {/* Tarjeta derecha */}
                     <div
@@ -75,7 +102,7 @@ export default function Home() {
                             className="h-2/3 rounded-tr-xl p-4 bg-gray-200"
                             style={{ height: '60%' }}
                         >
-                            <p className="text-normal font-semibold">
+                            <p className="text-base font-semibold">
                                 Hec7orci7o
                             </p>
                         </div>
