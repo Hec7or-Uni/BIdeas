432d480f4d96b3e1f9e4201dd40bf52dfa2a426a
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-10-31 00:32:00 +0200

    new pages to make the fronted

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+export default function Login() {
+  return (
+    <div>
+      <div>
+        <form>
+          <input />
+          <input />
+          <button></button>
+        </form>
+      </div>
+      <div>
+        <p></p>
+        <button></button>
+      </div>
+    </div>
+  )
+}
