c0c3380affb2bcbc0246caa389b292293b541fd1
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-11-13 22:30:56 +0100

    legal

---
+++ pages/faq.js
@@ -0,0 +1,36 @@
+import LayoutOut from "../components/LayoutOut"
+import FAQ from "components/FAQ"
+
+export default function faq() {
+  return (
+    <div className="container px-16 py-8 ">
+      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 pb-10">
+        <h2 className="px-2 py-2 font-bold text-2xl capitalize mb-4">
+          frequently asked questions
+        </h2>
+        <FAQ
+          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+        <FAQ
+          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+        <FAQ
+          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+      </div>
+    </div>
+  )
+}
+
+faq.getLayout = function getLayout(page) {
+  return <LayoutOut>{page}</LayoutOut>
+}
--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -1,3 +1,264 @@
-export default function Login() {
-  return <div></div>
+import LayoutOut from "../components/LayoutOut"
+
+export default function privacypolicy() {
+  return (
+    <div className="container px-16 py-8 ">
+      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 space-y-5 pb-10">
+        <h1 className="px-2 py-2 font-bold text-2xl">
+          Privacy Policy for cogs
+        </h1>
+
+        <p className="text-justify">
+          At cogs, accessible from http://localhost:3000/, one of our main
+          priorities is the privacy of our visitors. This Privacy Policy
+          document contains types of information that is collected and recorded
+          by cogs and how we use it.
+        </p>
+
+        <p className="text-justify">
+          If you have additional questions or require more information about our
+          Privacy Policy, do not hesitate to contact us.
+        </p>
+
+        <p className="text-justify">
+          This Privacy Policy applies only to our online activities and is valid
+          for visitors to our website with regards to the information that they
+          shared and/or collect in cogs. This policy is not applicable to any
+          information collected offline or via channels other than this website.
+          Our Privacy Policy was created with the help of the{" "}
+          <a href="https://www.privacypolicygenerator.info">
+            Free Privacy Policy Generator
+          </a>
+          .
+        </p>
+
+        <h2 className="font-bold text-lg">Consent</h2>
+
+        <p className="text-justify">
+          By using our website, you hereby consent to our Privacy Policy and
+          agree to its terms.
+        </p>
+
+        <h2 className="font-bold text-lg">Information we collect</h2>
+
+        <p className="text-justify">
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at
+          the point we ask you to provide your personal information.
+        </p>
+        <p className="text-justify">
+          If you contact us directly, we may receive additional information
+          about you such as your name, email address, phone number, the contents
+          of the message and/or attachments you may send us, and any other
+          information you may choose to provide.
+        </p>
+        <p className="text-justify">
+          When you register for an Account, we may ask for your contact
+          information, including items such as name, company name, address,
+          email address, and telephone number.
+        </p>
+
+        <h2 className="font-bold text-lg">How we use your information</h2>
+
+        <p className="text-justify">
+          We use the information we collect in various ways, including to:
+        </p>
+
+        <ul>
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the website, and for
+            marketing and promotional purposes
+          </li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+        </ul>
+
+        <h2 className="font-bold text-lg">Log Files</h2>
+
+        <p className="text-justify">
+          cogs follows a standard procedure of using log files. These files log
+          visitors when they visit websites. All hosting companies do this and a
+          part of hosting services&apos; analytics. The information collected by
+          log files include internet protocol (IP) addresses, browser type,
+          Internet Service Provider (ISP), date and time stamp, referring/exit
+          pages, and possibly the number of clicks. These are not linked to any
+          information that is personally identifiable. The purpose of the
+          information is for analyzing trends, administering the site, tracking
+          users&apos; movement on the website, and gathering demographic
+          information.
+        </p>
+
+        <h2 className="font-bold text-lg">Cookies and Web Beacons</h2>
+
+        <p className="text-justify">
+          Like any other website, cogs uses &quot;cookies&quot;. These cookies
+          are used to store information including visitors&apos; preferences,
+          and the pages on the website that the visitor accessed or visited. The
+          information is used to optimize the users&apos; experience by
+          customizing our web page content based on visitors&apos; browser type
+          and/or other information.
+        </p>
+
+        <p className="text-justify">
+          For more general information on cookies, please read{" "}
+          <a href="https://www.cookieconsent.com/">
+            more on the Cookie Consent website
+          </a>
+          .
+        </p>
+
+        <h2 className="font-bold text-lg">Our Advertising Partners</h2>
+
+        <p className="text-justify">
+          Some of advertisers on our site may use cookies and web beacons. Our
+          advertising partners are listed below. Each of our advertising
+          partners has their own Privacy Policy for their policies on user data.
+          For easier access, we hyperlinked to their Privacy Policies below.
+        </p>
+
+        <ul>
+          <li>
+            <p className="text-justify">Google</p>
+            <p className="text-justify">
+              <a href="https://policies.google.com/technologies/ads">
+                https://policies.google.com/technologies/ads
+              </a>
+            </p>
+          </li>
+        </ul>
+
+        <h2 className="font-bold text-lg">
+          Advertising Partners Privacy Policies
+        </h2>
+
+        <p className="text-justify">
+          You may consult this list to find the Privacy Policy for each of the
+          advertising partners of cogs.
+        </p>
+
+        <p className="text-justify">
+          Third-party ad servers or ad networks uses technologies like cookies,
+          JavaScript, or Web Beacons that are used in their respective
+          advertisements and links that appear on cogs, which are sent directly
+          to users&apos; browser. They automatically receive your IP address
+          when this occurs. These technologies are used to measure the
+          effectiveness of their advertising campaigns and/or to personalize the
+          advertising content that you see on websites that you visit.
+        </p>
+
+        <p className="text-justify">
+          Note that cogs has no access to or control over these cookies that are
+          used by third-party advertisers.
+        </p>
+
+        <h2 className="font-bold text-lg">Third Party Privacy Policies</h2>
+
+        <p className="text-justify">
+          cogs&apos;s Privacy Policy does not apply to other advertisers or
+          websites. Thus, we are advising you to consult the respective Privacy
+          Policies of these third-party ad servers for more detailed
+          information. It may include their practices and instructions about how
+          to opt-out of certain options.{" "}
+        </p>
+
+        <p className="text-justify">
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers&apos;
+          respective websites.
+        </p>
+
+        <h2 className="font-bold text-lg">
+          CCPA Privacy Rights (Do Not Sell My Personal Information)
+        </h2>
+
+        <p className="text-justify">
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+        </p>
+        <p className="text-justify">
+          Request that a business that collects a consumer&apos;s personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers.
+        </p>
+        <p className="text-justify">
+          Request that a business delete any personal data about the consumer
+          that a business has collected.
+        </p>
+        <p className="text-justify">
+          Request that a business that sells a consumer&apos;s personal data,
+          not sell the consumer&apos;s personal data.
+        </p>
+        <p className="text-justify">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </p>
+
+        <h2 className="font-bold text-lg">GDPR Data Protection Rights</h2>
+
+        <p className="text-justify">
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+        </p>
+        <p className="text-justify">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+        </p>
+        <p className="text-justify">
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+        </p>
+        <p className="text-justify">
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+        </p>
+        <p className="text-justify">
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions.
+        </p>
+        <p className="text-justify">
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+        </p>
+        <p className="text-justify">
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions.
+        </p>
+        <p className="text-justify">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </p>
+
+        <h2 className="font-bold text-lg">Children&apos;s Information</h2>
+
+        <p className="text-justify">
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity.
+        </p>
+
+        <p className="text-justify">
+          cogs does not knowingly collect any Personal Identifiable Information
+          from children under the age of 13. If you think that your child
+          provided this kind of information on our website, we strongly
+          encourage you to contact us immediately and we will do our best
+          efforts to promptly remove such information from our records.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+privacypolicy.getLayout = function getLayout(page) {
+  return <LayoutOut>{page}</LayoutOut>
 }
