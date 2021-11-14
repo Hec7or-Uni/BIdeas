b22a26226e66cd43c7427e4b30e2d0fcf906b6b1
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-11-15 00:04:12 +0100

    index cambiado y sticky navbar

--- components/Navegation/NavbarOut.js
+++ components/Navegation/NavbarOut.js
@@ -3,7 +3,7 @@ import { signOut } from "next-auth/react"
 
 export default function Navbar({ session }) {
   return (
-    <div className="h-16 w-full flex items-center tracking-wide bg-gray-200 z-50">
+    <div className="sticky top-0 h-16 w-full flex items-center tracking-wide bg-gray-200 z-50">
       <div className="flex justify-center container mx-auto relative">
         <div className="absolute left-0 mx-4">logo</div>
         <nav>enlaces</nav>
