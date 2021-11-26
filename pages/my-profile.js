2432a2738e499e51f5451cd0d1b63e9154f1089c
Ismati5 <796919@unizar.es>
2021-11-26 19:41:59 +0100

    Reorganizacion my-profile

--- pages/my-profile.js
+++ pages/my-profile.js
@@ -14,7 +14,16 @@ import {
   FiFlag,
   FiBriefcase,
   FiHeart,
+  FiSave,
 } from "react-icons/fi"
+import {
+  BsTwitter,
+  BsFacebook,
+} from "react-icons/bs"
+
+import {
+  CgWebsite,
+} from "react-icons/cg"
 
 export default function Profile({ user, owns, participates }) {
   const [isActive, setActive] = useState(1)
@@ -205,106 +214,63 @@ export default function Profile({ user, owns, participates }) {
       {isActive === 2 && (
         <div>
           {/* Profile Avatar Edit */}
-          <div className="px-8 mt-5">
-            <p className="text-base font-bold">Profile Avatar</p>
-          </div>
 
-          <div className="flex gap-x-48 mt-5 items-center">
-            <div className="flex gap-x-12 px-6 items-center">
-              <div className="flex w-32 h-32 rounded-full items-center justify-center">
-                <img
-                  src={user.avatar || "/personas/DefaultAvatar.jpg"}
-                  className="w-32 h-32 rounded-full object-cover relative"
-                />
-                {/* <div className="flex h-32 w-32 rounded-full absolute justify-center opacity-0 hover:opacity-90">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mt-12"
-                  >
-                    <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
-                    <circle cx="12" cy="13" r="4"></circle>
-                  </svg>
-                </div> */}
-              </div>
+          <div className="px-8 mt-8 w-4/6">
+            <p className="text-base font-bold dark:text-gray-100">Profile Avatar</p>
+            <div className="mt-4 px-2">
+              <div className="flex items-center gap-x-4 w-full h-full">
+                
+                {/* Parte izquierda img, input*/}
+                <div className="flex w-full items-center">
 
-              <label>
-                <span className="text-xs font-semibold uppercase">
-                  avatar url
-                </span>
-                <div>
-                  <input
-                    id="avatarUrl"
-                    type="url"
-                    name="avatarUrl"
-                    placeholder={user.avatar || "https://avatar..."}
-                    onChange={handleAvatar}
-                    className="block w-72 px-3 py-2 mt-1 text-gray-700 border rounded-md form-input focus:border-blue-600"
-                  />
-                </div>
-              </label>
-
-              {/* <button className="h-7 w-32 border-2 border-black text-xs font-medium uppercase rounded-sm" Onclick="document.getElementById('file-input').click();">
-                <div className="flex gap-x-2 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`h-3 w-3 ml-1 transform rotate-180`}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M19 9l-7 7-7-7"
+                  <div className="flex w-1/2 justify-start">
+                    <img
+                      src={user.avatar || "/personas/DefaultAvatar.jpg"}
+                      className="w-32 h-32 rounded-full object-cover relative"
                     />
-                  </svg>
-                  upload avatar
+                  </div>
+
+
+                  <div className="w-full">
+                    <span className="text-xs font-semibold uppercase dark:text-gray-100">
+                      avatar url
+                    </span>
+                    <input
+                      id="avatarUrl"
+                      type="url"
+                      name="avatarUrl"
+                      placeholder={user.avatar || "https://avatar.com"}
+                      onChange={handleAvatar}
+                      className="w-full py-2 px-2 mt-1 text-gray-700 border rounded-md focus:border-blue-600"
+                    />
+                  </div>
                 </div>
-              </button>} */}
-            </div>
-            <div>
-              <button
-                type="submit"
-                form="form-profile"
-                className="h-10 w-40 border-0 bg-indigo-500 text-white text-bold font-medium uppercase rounded-full"
-              >
-                <div className="flex gap-x-2 ml-2 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
-                    <polyline points="17 21 17 13 7 13 7 21"></polyline>
-                    <polyline points="7 3 7 8 15 8"></polyline>
-                  </svg>
-                  save changes
+
+                {/* Parte derecha save changes*/}
+                <div className="flex w-full items-center">
+                  <div className="flex w-full h-16 justify-center">
+                    <button
+                      type="submit"
+                      form="form-profile"
+                      className="w-5/12 bg-green-600 text-white text-bold font-medium uppercase rounded-md">
+                        <div className="flex-col items-center justify-center">
+                          <div className="flex w-full justify-center">
+                            <FiSave className="h-6 w-6 justify-center items-center text-neutral" />
+                          </div>
+                          save changes
+                        </div>
+                    </button>
+                  </div>
                 </div>
-              </button>
+              </div>
             </div>
           </div>
 
           {/* General Info Edit */}
-          <div className="px-8 mt-8 w-5/6">
+          <div className="px-8 mt-8 w-4/6">
             <p className="text-base font-bold dark:text-gray-100">General Information</p>
             <div className="mt-4 px-2">
               <form id="form-profile">
-
                 {/* Name & LastName */}
                 <div className="flex gap-x-4 w-full">
                   <div className="w-full">
@@ -396,7 +362,7 @@ export default function Profile({ user, owns, participates }) {
                     />
                   </div>
                 </div>
-                <div className="flex w-full">
+                <div className="flex w-full mt-2">
                   <div className="w-full">
                     <span className="text-xs font-semibold uppercase dark:text-gray-100">
                       profile description
@@ -416,64 +382,63 @@ export default function Profile({ user, owns, participates }) {
           </div>
 
           {/* Social Media */}
-          <div className="px-8 mt-8 mb-12">
-            <p className="text-base font-bold">Social Media</p>
-            <div className="mt-4">
-              <form className="">
-                <div>
-                  <label>
-                    <span className="text-xs font-semibold uppercase block">
-                      website
-                    </span>
-                    <div>
-                      <input
-                        id="website"
-                        type="url"
-                        name="website"
-                        placeholder={user.website || "https//www.yourweb.com"}
-                        onChange={handleWebsite}
-                        className="block w-96 px-3 py-2 mt-1 text-gray-700 border rounded-md form-inpu focus:border-blue-600"
-                      />
+          <div className="px-8 mt-8 w-4/6">
+            <p className="text-base font-bold dark:text-gray-100">Social media</p>
+            <div className="mt-4 px-2">
+              <div className="flex gap-x-4 w-full">
+                <div className="w-full">
+                <div className="flex gap-x-1">
+                      <CgWebsite className="h-auto w-auto object-fill object-center text-gray-400" />
+                      <span className="text-xs font-semibold uppercase dark:text-gray-100">
+                        website
+                      </span>
                     </div>
-                  </label>
+                  <input
+                    id="website"
+                    type="url"
+                    name="website"
+                    placeholder={user.website || "https//www.yourweb.com"}
+                    onChange={handleWebsite}
+                    className="block w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:border-blue-600"
+                  />
                 </div>
-                <div className="flex gap-x-4">
-                  <div>
-                    <label>
-                      <span className="text-xs font-semibold uppercase block mt-4">
+                <div className="w-full">
+                </div>
+              </div>
+              <div className="flex gap-x-4 w-full">
+                <div className="w-full mt-4">
+                    <div className="flex gap-x-1">
+                      <BsTwitter className="h-auto w-auto object-fill object-center text-blue-600" />
+                      <span className="text-xs font-semibold uppercase dark:text-gray-100">
                         twitter
                       </span>
-                      <div>
-                        <input
-                          id="twitter"
-                          type="text"
-                          name="twitter"
-                          placeholder={user.twitter || "@TwitterUser"}
-                          onChange={handleTwitter}
-                          className="block w-96 px-3 py-2 mt-1 text-gray-700 border rounded-md form-inpu focus:border-blue-600"
-                        />
-                      </div>
-                    </label>
-                  </div>
-                  <div>
-                    <label>
-                      <span className="text-xs font-semibold uppercase block mt-4">
+                    </div>
+                    <input
+                      id="twitter"
+                      type="text"
+                      name="twitter"
+                      placeholder={user.twitter || "@TwitterUser"}
+                      onChange={handleTwitter}
+                      className="block w-full px-3 py-2 mt-1 text-gray-700 border rounded-md form-inpu focus:border-blue-600"
+                    />
+                </div>
+                <div className="w-full mt-4">
+                    <div className="flex gap-x-1">
+                      <BsFacebook className="h-auto w-auto object-fill object-center text-blue-700" />
+                      <span className="text-xs font-semibold uppercase dark:text-gray-100">
                         facebook
                       </span>
-                      <div>
-                        <input
-                          id="facebook"
-                          type="text"
-                          name="facebook"
-                          placeholder={user.facebook || "FacebookUser"}
-                          onChange={handleFacebook}
-                          className="block w-96 px-3 py-2 mt-1 text-gray-700 border rounded-md form-inpu focus:border-blue-600"
-                        />
-                      </div>
-                    </label>
-                  </div>
+                    </div>
+                    <input
+                      id="facebook"
+                      type="text"
+                      name="facebook"
+                      placeholder={user.facebook || "FacebookUser"}
+                      onChange={handleFacebook}
+                      className="block w-full px-3 py-2 mt-1 mb-5 text-gray-700 border rounded-md form-inpu focus:border-blue-600"
+                    />
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
