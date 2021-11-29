aec0bfa69e950d20feec1962327ac6ce7cc5105c
hec7orci7o <hectortoraltrabajos@gmail.com>
2021-11-29 22:25:42 +0100

    order by en users y projects

--- pages/faq.js
+++ pages/faq.js
@@ -3,25 +3,43 @@ import LayoutOut from "../components/LayoutOut"
 
 export default function faq() {
   return (
-    <div className="container px-16 py-8 ">
-      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 pb-10">
-        <h2 className="px-2 py-2 font-bold text-2xl capitalize mb-4">
+    <div className="container px-16 py-8 min-h-screen -mt-16">
+      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 pb-10 mt-24">
+        <h2 className="px-2 py-2 font-bold text-2xl capitalize mb-8 dark:text-gray-100">
           frequently asked questions
         </h2>
         <FAQ
-          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          title={"What is BIdeas?"}
           content={
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
           }
         />
         <FAQ
-          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          title={"Why BIdeas?"}
           content={
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
           }
         />
         <FAQ
-          title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+          title={"How to create a team?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+        <FAQ
+          title={"How to join a team?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+        <FAQ
+          title={"how to find the best projects?"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
+          }
+        />
+        <FAQ
+          title={"How to report a bug?"}
           content={
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet venenatis urna cursus. Eleifend quam adipiscing vitae proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Tortor at risus viverra adipiscing at in tellus integer feugiat. Et odio pellentesque diam volutpat commodo sed egestas."
           }
--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -3,23 +3,20 @@ import LayoutOut from "../components/LayoutOut"
 export default function privacypolicy() {
   return (
     <div className="container px-16 py-8 ">
-      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 space-y-5 pb-10">
-        <h1 className="px-2 py-2 font-bold text-2xl">
+      <div className="mx-auto flex flex-wrap items-center justify-center w-1/2 space-y-5 pb-10 dark:text-gray-300">
+        <h1 className="dark:text-white px-2 py-2 font-bold text-2xl">
           Privacy Policy for cogs
         </h1>
-
         <p className="text-justify">
           At cogs, accessible from http://localhost:3000/, one of our main
           priorities is the privacy of our visitors. This Privacy Policy
           document contains types of information that is collected and recorded
           by cogs and how we use it.
         </p>
-
         <p className="text-justify">
           If you have additional questions or require more information about our
           Privacy Policy, do not hesitate to contact us.
         </p>
-
         <p className="text-justify">
           This Privacy Policy applies only to our online activities and is valid
           for visitors to our website with regards to the information that they
@@ -31,16 +28,14 @@ export default function privacypolicy() {
           </a>
           .
         </p>
-
-        <h2 className="font-bold text-lg">Consent</h2>
-
+        <h2 className="dark:text-white font-bold text-lg mr-1">Consent</h2>
         <p className="text-justify">
           By using our website, you hereby consent to our Privacy Policy and
           agree to its terms.
         </p>
-
-        <h2 className="font-bold text-lg">Information we collect</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          Information we collect
+        </h2>
         <p className="text-justify">
           The personal information that you are asked to provide, and the
           reasons why you are asked to provide it, will be made clear to you at
@@ -57,13 +52,12 @@ export default function privacypolicy() {
           information, including items such as name, company name, address,
           email address, and telephone number.
         </p>
-
-        <h2 className="font-bold text-lg">How we use your information</h2>
-
+        <h2 className="dark:text-white font-bold text-lg mr-1">
+          How we use your information
+        </h2>
         <p className="text-justify">
           We use the information we collect in various ways, including to:
         </p>
-
         <ul>
           <li>Provide, operate, and maintain our website</li>
           <li>Improve, personalize, and expand our website</li>
@@ -78,9 +72,7 @@ export default function privacypolicy() {
           <li>Send you emails</li>
           <li>Find and prevent fraud</li>
         </ul>
-
-        <h2 className="font-bold text-lg">Log Files</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">Log Files</h2>
         <p className="text-justify">
           cogs follows a standard procedure of using log files. These files log
           visitors when they visit websites. All hosting companies do this and a
@@ -93,9 +85,9 @@ export default function privacypolicy() {
           users&apos; movement on the website, and gathering demographic
           information.
         </p>
-
-        <h2 className="font-bold text-lg">Cookies and Web Beacons</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          Cookies and Web Beacons
+        </h2>
         <p className="text-justify">
           Like any other website, cogs uses &quot;cookies&quot;. These cookies
           are used to store information including visitors&apos; preferences,
@@ -104,7 +96,6 @@ export default function privacypolicy() {
           customizing our web page content based on visitors&apos; browser type
           and/or other information.
         </p>
-
         <p className="text-justify">
           For more general information on cookies, please read{" "}
           <a href="https://www.cookieconsent.com/">
@@ -112,16 +103,15 @@ export default function privacypolicy() {
           </a>
           .
         </p>
-
-        <h2 className="font-bold text-lg">Our Advertising Partners</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          Our Advertising Partners
+        </h2>
         <p className="text-justify">
           Some of advertisers on our site may use cookies and web beacons. Our
           advertising partners are listed below. Each of our advertising
           partners has their own Privacy Policy for their policies on user data.
           For easier access, we hyperlinked to their Privacy Policies below.
         </p>
-
         <ul>
           <li>
             <p className="text-justify">Google</p>
@@ -132,16 +122,13 @@ export default function privacypolicy() {
             </p>
           </li>
         </ul>
-
-        <h2 className="font-bold text-lg">
+        <h2 className="ml-4 dark:text-white font-bold text-lg">
           Advertising Partners Privacy Policies
         </h2>
-
         <p className="text-justify">
           You may consult this list to find the Privacy Policy for each of the
           advertising partners of cogs.
         </p>
-
         <p className="text-justify">
           Third-party ad servers or ad networks uses technologies like cookies,
           JavaScript, or Web Beacons that are used in their respective
@@ -151,14 +138,13 @@ export default function privacypolicy() {
           effectiveness of their advertising campaigns and/or to personalize the
           advertising content that you see on websites that you visit.
         </p>
-
         <p className="text-justify">
           Note that cogs has no access to or control over these cookies that are
           used by third-party advertisers.
         </p>
-
-        <h2 className="font-bold text-lg">Third Party Privacy Policies</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          Third Party Privacy Policies
+        </h2>
         <p className="text-justify">
           cogs&apos;s Privacy Policy does not apply to other advertisers or
           websites. Thus, we are advising you to consult the respective Privacy
@@ -166,18 +152,15 @@ export default function privacypolicy() {
           information. It may include their practices and instructions about how
           to opt-out of certain options.{" "}
         </p>
-
         <p className="text-justify">
           You can choose to disable cookies through your individual browser
           options. To know more detailed information about cookie management
           with specific web browsers, it can be found at the browsers&apos;
           respective websites.
         </p>
-
-        <h2 className="font-bold text-lg">
+        <h2 className="dark:text-white font-bold text-lg">
           CCPA Privacy Rights (Do Not Sell My Personal Information)
         </h2>
-
         <p className="text-justify">
           Under the CCPA, among other rights, California consumers have the
           right to:
@@ -199,9 +182,9 @@ export default function privacypolicy() {
           If you make a request, we have one month to respond to you. If you
           would like to exercise any of these rights, please contact us.
         </p>
-
-        <h2 className="font-bold text-lg">GDPR Data Protection Rights</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          GDPR Data Protection Rights
+        </h2>
         <p className="text-justify">
           We would like to make sure you are fully aware of all of your data
           protection rights. Every user is entitled to the following:
@@ -238,15 +221,14 @@ export default function privacypolicy() {
           If you make a request, we have one month to respond to you. If you
           would like to exercise any of these rights, please contact us.
         </p>
-
-        <h2 className="font-bold text-lg">Children&apos;s Information</h2>
-
+        <h2 className="dark:text-white font-bold text-lg">
+          Children&apos;s Information
+        </h2>
         <p className="text-justify">
           Another part of our priority is adding protection for children while
           using the internet. We encourage parents and guardians to observe,
           participate in, and/or monitor and guide their online activity.
         </p>
-
         <p className="text-justify">
           cogs does not knowingly collect any Personal Identifiable Information
           from children under the age of 13. If you think that your child
